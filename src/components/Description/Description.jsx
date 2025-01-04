6e0181af2fcd45ce2cf70b12ec641d57056b854e
Kos <kosnesterenko@gmail.com>
2025-01-05 00:33:59 +0200

    init

--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,11 +1,7 @@
 const Description = () => {
   return (
     <div>
-      <h1>Sip Happens Café</h1>
-      <p>
-        Please leave your feedback about our service by selecting one of the
-        options below.
-      </p>
+      <h1>Phonebook</h1>
     </div>
   );
 };
